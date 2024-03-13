edc09c54444afdec963f81fd70df78cd1eab1c5d
mbhaskarreddy1985 <mbhaskarreddy1985@outlook.com>
2024-03-13 22:16:25 +0530

    Made changes in the file

--- adding.c
+++ adding.c
@@ -8,7 +8,7 @@ int main() {
 
     // calculate the sum
     sum = number1 + number2;      
-    
+    printf("The result is:");
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
